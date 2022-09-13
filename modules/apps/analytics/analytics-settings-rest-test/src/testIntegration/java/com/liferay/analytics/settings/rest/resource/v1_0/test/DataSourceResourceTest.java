fce3e22295bcc13b66fafa1c8fcb8833f6c43d77
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-09-13 16:08:43 +0200

    LRAC-11869 analytics-settings-rest buildREST

---
+++ modules/apps/analytics/analytics-settings-rest-test/src/testIntegration/java/com/liferay/analytics/settings/rest/resource/v1_0/test/DataSourceResourceTest.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.analytics.settings.rest.resource.v1_0.test;
+
+import com.liferay.analytics.settings.rest.client.dto.v1_0.DataSourceToken;
+import com.liferay.analytics.settings.rest.client.http.HttpInvoker;
+import com.liferay.analytics.settings.rest.internal.helper.v1_0.AnalyticsCloudClientHelper;
+import com.liferay.arquillian.extension.junit.bridge.junit.Arquillian;
+
+import com.liferay.portal.kernel.json.JSONUtil;
+import com.liferay.portal.kernel.test.util.RandomTestUtil;
+import org.junit.Ignore;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+
+/**
+ * @author Riccardo Ferrari
+ */
+@Ignore
+@RunWith(Arquillian.class)
+public class DataSourceResourceTest extends BaseDataSourceResourceTestCase {
+
+}
