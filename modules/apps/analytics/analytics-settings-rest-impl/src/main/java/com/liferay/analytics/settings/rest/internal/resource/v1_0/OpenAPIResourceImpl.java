904d5389cf01d669accde87b9f43564b09d0362a
Riccardo Ferrari <riccardo.ferrari@liferay.com>
2022-10-26 12:07:36 +0200

    LRAC-12207 analytics-settings-rest buildREST

--- modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/resource/v1_0/OpenAPIResourceImpl.java
+++ modules/apps/analytics/analytics-settings-rest-impl/src/main/java/com/liferay/analytics/settings/rest/internal/resource/v1_0/OpenAPIResourceImpl.java
@@ -102,6 +102,8 @@ public class OpenAPIResourceImpl {
 
 			add(CommerceChannelResourceImpl.class);
 
+			add(ContactResourceImpl.class);
+
 			add(DataSourceResourceImpl.class);
 
 			add(SiteResourceImpl.class);
